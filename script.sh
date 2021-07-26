0ce3cc81380da7222f62f4c137c62807d31d3fc6
Tamires <tamires.aquino@terceiro-sky.com.br>
2021-07-26 15:16:08 -0300

    Teste semver:patch

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
-# #!/bin/bash
-# fullTag=$(git tag --sort version:refname | tail -1)
-# patch=$(echo ${fullTag} | grep -zoP '(?<=\.)[^.]*$')
-# newPatch="$(($patch + 1))"
-# # 3 -> 4
-# echo "${fullTag/$patch/$newPatch}"
-# # 2.0.0 -> 2.0.1
-# # 2.0.0 -> 2.1.0
+# # #!/bin/bash
+# # fullTag=$(git tag --sort version:refname | tail -1)
+# # patch=$(echo ${fullTag} | grep -zoP '(?<=\.)[^.]*$')
+# # newPatch="$(($patch + 1))"
+# # # 3 -> 4
+# # echo "${fullTag/$patch/$newPatch}"
+# # # 2.0.0 -> 2.0.1
+# # # 2.0.0 -> 2.1.0
  
-# echo fullTag ${fullTag}
-# echo patch ${patch}
-# echo newPatch ${newPatch}
+# # echo fullTag ${fullTag}
+# # echo patch ${patch}
+# # echo newPatch ${newPatch}
 
 #!/bin/sh
 
