02e16580a7572b74a49c5612754dd6ab6ed789b1
Tamires <tamires.aquino@terceiro-sky.com.br>
2021-07-27 14:36:09 -0300

    Validando a necessidade das variáveis gitCommit e needsTag

--- script.sh
+++ script.sh
@@ -2,9 +2,7 @@
 
 versionTag=""
 
-# ./script.sh -v patch
-# ./script.sh -v minor
-# ./script.sh -v major
+# ./script.sh -v patch or minor or major
 #get parameters
 while getopts v: flag
 do
@@ -12,8 +10,6 @@ do
     v) versionTag=${OPTARG};;
   esac
 done
-
-# echo 'versiontag' $versionTag
 
 # fullTag=`git describe --abbrev=0 --tags 2>/dev/null`
 fullTag=$(git tag --sort version:refname | tail -1)
@@ -32,6 +28,7 @@ fi
 newTag="$versionNumber1.$versionNumber2.$versionNumber3"
 echo "($versionTag) updating $fullTag to $newTag"
 
+#it sees if the last commit has a tag or not. If it hasn't, it associates the newTag
 gitCommit=`git rev-parse HEAD`
 needsTag=`git describe --contains $gitCommit 2>/dev/null`
 
